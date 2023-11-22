d12c97651424630399a9b43e91fd97b4c149431c
albarmo <moerhamsa@gmail.com>
2023-11-22 21:26:40 +0700

    feat:fix layouting homepage

--- src/app/components/jumbo-article.tsx
+++ src/app/components/jumbo-article.tsx
@@ -3,17 +3,18 @@ import Image from "next/image";
 
 const JumboArticle = () => {
     return (
-        <div className="flex px-20 py-10 justify-between items-center space-x-20 bg-[#F5F4F4]">
-            <div className="w-1/2 flex flex-col justify-start space-y-10">
-                <h1 className="text-2xl font-semibold w-52">ACTIVITY REPORT <span className="text-red-600">.</span></h1>
-                <h2 className="font-semibold text-4xl leading-none w-full">EXPEDISI JAVA ANDESIT MARATHON 2023</h2>
-                <p className="w-full text-md text-gray-400">Setelah menyelesaikan perintisan jalur multi pitch ditebing lawe Banjarnegara, Jawa Tengah, Tim java andesit marathon bergeser ke tebing sepikul Jawa Timur. berangkat menggunakan mobil carteran dari teman Mapala Palmasti banjarnegara (Rhino), Sampai ditebing sepikul sekitar pukul 05.00 pagi. eky dan abi langsung mengurus perizinan dari kantor desa, kepolisian, puskesmas, dan (tni), sedangkan mas hafri stay di kaki tebing untuk menyiapkan camp darat. Pada etape ini Alvin Reggy Pratama(egi) bisa menemani kegiatan pemanjatan karna etape sebelumnya ia tidak bisa menemani karna ada urusan kerja.</p>
+        <main className="bg-[#F5F4F4] flex flex-row max-w-full sm:flex-col md:flex-col-reverse lg:flex-row justify-around  ">
+            <div className="bg-red-400 basis-1/2 flex flex-col  justify-start space-y-10 m-10">
+                <h1 className="text-3xl w-40 sm:hidden md:hidden lg:block ">ACTIVITY REPORT <span className="text-red-600">.</span></h1>
+                <h2 className="w-full font-semibold text-5xl leading-none ">EXPEDISI JAVA <tr></tr>ANDESIT MARATHON <tr></tr>2023</h2>
+                <p className="w-ful text-md text-slate-600">Setelah menyelesaikan perintisan jalur multi pitch ditebing lawe Banjarnegara, Jawa Tengah, Tim java andesit marathon bergeser ke tebing sepikul Jawa Timur. berangkat menggunakan mobil carteran dari teman Mapala Palmasti banjarnegara (Rhino), Sampai ditebing sepikul sekitar pukul 05.00 pagi. eky dan abi langsung mengurus perizinan dari kantor desa, kepolisian, puskesmas, dan (tni), sedangkan mas hafri stay di kaki tebing untuk menyiapkan camp darat. Pada etape ini Alvin Reggy Pratama(egi) bisa menemani kegiatan pemanjatan karna etape sebelumnya ia tidak bisa menemani karna ada urusan kerja.</p>
                 <button className="w-44 h-10 bg-[#F92424] text-white font-semibold rounded-md">Read More</button>
             </div>
-            <div>
-                <Image src={'/images/manjat.png'} alt="climbing" width={621} height={621} className=" max-w-60  aspect-square object-cover shadow-md" />
+            <div className="bg-blue-400 m-10">
+                <h1 className="text-3xl w-40 mb-5 lg:hidden">ACTIVITY REPORT <span className="text-red-600">.</span></h1>
+                <Image src={'/images/manjat.png'} alt="climbing" width={621} height={621} className="object-contain" />
             </div>
-        </div >
+        </main>
     )
 }
 export default JumboArticle
